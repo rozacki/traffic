ddc0b9b3c0b74efb8e848f95821447192155264a
chris.rozacki <chris.rozacki@valtech.co.uk>
2020-05-01 19:33:59 +0100

    Add ingestion template Add git style command parser Add ingest function

--- pipeline.py
+++ pipeline.py
@@ -1,25 +1,31 @@
 import os
 from subprocess import Popen, PIPE
-import argparse
-from datetime import datetime
 import threading
 import shutil
+import tempfile
 
-from common import valid_date, get_sites
+from common import get_sites
 from download_sites_data import download_and_store_reports
 from maos import logger
 
-max_threads = 4*5
+'''
+How many threads can be run in parallel
+'''
+cpus = 4
+threads_per_cpu = 5
+max_threads = cpus*threads_per_cpu
 max_threads_semaphore = threading.Semaphore(max_threads)
+path_to_post_index_path_script = '/root/druid/apache - druid - 0.17.0/bin/post_index_path'
 
 
-def _get_thread_name(id, startdate, enddate):
-    return f'site-{id}-from-{startdate}-to-{enddate}'
+def _get_thread_name(site_id, startdate, enddate):
+    ''' create thread name from job details '''
+    return f'site-{site_id}-from-{startdate}-to-{enddate}'
 
 
 def run_process(cmd):
     '''
-    Download
+    runs an arbitrary process
     :param site:
     :param startdate:
     :param enddate:
@@ -39,7 +45,34 @@ def run_process(cmd):
         return False
 
 
+def ingest(datasource_name, source_folder, ingestion_template_file_name='ingestion_template.json',
+           url='http://localhost:8081'):
+    '''
+    Ingest data from the folder
+    :param source_folder:
+    :return:
+    '''
+    template = os.path.join('ingestions', ingestion_template_file_name)
+    logger.info(f'used {template} template to post the task')
+    with open(template) as f:
+        template = f.read()
+    task_string = template.format(datasource_name=datasource_name, source_folder=source_folder)
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write(str.encode(task_string))
+    logger.info(f'task definition stored in {tmp.name}')
+    cmd = [path_to_post_index_path_script,'--file', tmp.name, '--url', url]
+    logger.info(f'{cmd}')
+    run_process(cmd)
+
+
 def wrap_download_and_store_reports(site, startdate, enddate):
+    '''
+    This is where thread starts. Here we control number of threads running simultaneously
+    :param site:
+    :param startdate:
+    :param enddate:
+    :return:
+    '''
     current_thread = threading.current_thread()
     logger.info(f'starting thread {current_thread.getName()}')
     max_threads_semaphore.acquire()
@@ -89,13 +122,15 @@ def download_reports_async(site, sites_count, startdate, enddate, download_folde
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--site', type=int, default=1, help='Seek and start downloading from this site')
-    parser.add_argument('--sites-count', type=int, default=1, help='How many sites try to download. '
-                                                                   'Note some data may no be available for site')
-    parser.add_argument('--sites-file', type=str, default='sites_enriched_roads.csv')
-    parser.add_argument('-s', '--startdate', help="The Start Date - format YYYY-MM-DD", type=valid_date)
-    parser.add_argument('-e', '--enddate', help="The Stop Date - format YYYY-MM-DD", type=valid_date,
-                        default=datetime.now().strftime('%Y-%m-%d'))
-    args = parser.parse_args()
-    download_reports_async(args.site, args.sites_count, args.startdate, args.enddate)
+    pass
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--site', type=int, default=1, help='Seek and start downloading from this site')
+    # parser.add_argument('--sites-count', type=int, default=1, help='How many sites try to download. '
+    #                                                                'Note some data may no be available for site')
+    # parser.add_argument('--sites-file', type=str, default='sites_enriched_roads.csv')
+    # parser.add_argument('-s', '--startdate', help="The Start Date - format YYYY-MM-DD", type=valid_date)
+    # parser.add_argument('-e', '--enddate', help="The Stop Date - format YYYY-MM-DD", type=valid_date,
+    #                     default=datetime.now().strftime('%Y-%m-%d'))
+    # args = parser.parse_args()
+    # #download_reports_async(args.site, args.sites_count, args.startdate, args.enddate)
+
