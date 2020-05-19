b554488f35e0862d93b3e64def55d21acf8e0433
chris.rozacki <chris.rozacki@valtech.co.uk>
2020-05-19 22:15:34 +0100

    extend sys.path to handle maos modules

--- maos/pipelines/download_and_ingest.py
+++ maos/pipelines/download_and_ingest.py
@@ -1,3 +1,26 @@
+import logging
+
+def setup_environment():
+    import sys, os
+    # hardcoded segments
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    # remove first empty segment
+    segments = cwd.split('/')[1:]
+    # join back first 5 segments as this is where repo starts
+    sys_path = os.path.join('/', *segments[:5])
+    # extract 4th segment as global variable
+    environment = segments[3:4][0]
+    sys.path.insert(0, sys_path)
+    logging.info('new sys path is {}'.format(sys_path))
+    ofgem_libs = [key for key in sys.modules.keys() if 'ofgem' in key]
+    for i in ofgem_libs:
+        del (sys.modules[i])
+    logging.info('removed `ofgem` libraries')
+    return environment
+
+environment = setup_environment()
+logging.info(f'new environment is {environment}')
+
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
