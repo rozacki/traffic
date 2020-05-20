b411fc24cdd7eb2af7e23f9a4848c8a657c94b4e
chris.rozacki <chris.rozacki@valtech.co.uk>
2020-05-20 21:23:27 +0100

    config folder from airflow variables

--- maos/common.py
+++ maos/common.py
@@ -10,6 +10,12 @@ base_site_data_folder = 'data/sites'
 base_road_data_folder = 'data/roads'
 
 
+def set_globals(new_configs_base_folder, new_base_site_data_folder):
+    global configs_base_folder, base_site_data_folder
+    configs_base_folder = new_configs_base_folder
+    base_site_data_folder = new_base_site_data_folder
+
+
 def get_logger(name='main'):
     #logging.config.fileConfig(os.path.join(configs_base_folder, 'logging.conf'))
     return logging.getLogger(name)
